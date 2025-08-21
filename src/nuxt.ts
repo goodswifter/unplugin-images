8177c2baecf3bb0bc6951f465030bb790ef152c8
钟爱东 <zhongaidong@wanlianyida.com>
2025-08-21 15:38:54 +0800

    fix: 报错问题

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,4 +1,4 @@
-import type { Options } from './types'
+import type { Options } from './lib/types'
 import { addVitePlugin, addWebpackPlugin, defineNuxtModule } from '@nuxt/kit'
 import vite from './vite'
 import webpack from './webpack'
